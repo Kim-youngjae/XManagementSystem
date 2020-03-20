04b6fad161848ae10b3dd519692d2e2aedd2c141
user <user@DESKTOP-2KR5IV5>
2020-03-20 13:26:25 +0900

    create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	public static void main (String[] args) {
+		System.out.println("*** Cloth Management System Menu ***");
+		System.out.println("	1. Add Clothes");
+		System.out.println("	2. Delete Clothes");
+		System.out.println("	3. Edit Clothes");
+		System.out.println("	4. View Clothes");
+		System.out.println("	5. Exit");
+		System.out.println("Select one number between 1-5 :");
+
+	}
+}
+// 옷장 관리 시스템
